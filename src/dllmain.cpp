b73e9c77564b769d1d49fb5568e33c91265e6582
Lyall <lyall@stamper.net>
2026-06-09 20:59:19 +0100

    Version bump.

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -13,7 +13,7 @@ HMODULE thisModule;
 
 // Fix details
 std::string sFixName = "MGSVFix";
-std::string sFixVersion = "0.0.2";
+std::string sFixVersion = "0.0.3";
 std::filesystem::path sFixPath;
 
 // Ini
